d3b66029ca86b4564afe0c87868cabcd0193e6f4
Tom McKearney <thomas.mckearney@appliedis.com>
2016-03-11 08:11:23 -0500

    Change to real battery monitoring
    The getVcc was getting post-regulator voltage, which didn't help, so had
    to switch to regular reading.
    Also decided 5 readings from the battery were useful because it was
    varying so much
    Added ifdefs around logic to allow relative quick look for battery test,
    will refactor later

--- esp8266TempHumidity.ino
+++ esp8266TempHumidity.ino
@@ -8,41 +8,81 @@ const char* password = "";
 const char* hostName = "Hoopy"; // TODO: CHANGE FOR EACH DEVICE!
  
 const char* host = "192.168.0.106";
+const int httpPort = 8080;
+
+//#define BATTERY_TEST
 
 #define DHTTYPE DHT22
 #define DHTPIN  12
 
-ADC_MODE(ADC_VCC);
+#define POLL_INTERVAL_MICROSECONDS 15*60*1000000  // Minutes * 60 seconds * 1 million 
 
 DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
-float humidity, temp_f;  // Values read from sensor
 
-float batteryLevel = -1.0;
+const int numReadings = 5;
+float temps[numReadings];
+float hums[numReadings];
+float batteryLevels[numReadings];
 
+void getTempHum() {
+  float temp = -1;
+  float humidity = -1;
 
-void getTemperature() {
- 
-  // Reading temperature for humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-  humidity = dht.readHumidity();          // Read humidity (percent)
-  temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(humidity) || isnan(temp_f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  for (int i=0; i < numReadings; i++) {
+#ifndef BATTERY_TEST
+    delay(2000);
+    
+    temp = dht.readTemperature(true);     // Read temperature as Fahrenheit
+    if (isnan(temp)) {
+      temp = -1;
+    }
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    if (isnan(humidity)) {
+      humidity = -1;
+    }
+    Serial.print("Temp/Hum: ");
+    Serial.print(temp);
+    Serial.print("/");
+    Serial.println(humidity);
+#endif
+
+    temps[i] = temp;
+    hums[i] = humidity;
   }
 }
 
 void readBatteryLevel() {
-  batteryLevel = ESP.getVcc();
+
+  for(int i=0; i < numReadings; ++i) {
+    int level = analogRead(A0);
+   
+    batteryLevels[i] = level;
+  }
 }
 
-void setup() {
-  Serial.begin(115200);
-  delay(100);
- 
-  // We start by connecting to a WiFi network
- 
+String arrayToJson(float arr[]) 
+{
+  String result = "[";
+  for(int i=0; i < numReadings; i++) 
+  {
+    float val = arr[i];
+    result += arr[i];
+    if (i < numReadings-1) {
+      result += ",";
+    }
+  }
+  result += "]";
+
+  return result;
+}
+
+void readAllAndReport() {
+  Serial.println("");
+
+  getTempHum();
+
+  readBatteryLevel();
+
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -59,27 +99,12 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-}
 
-void readAllAndReport() {
-  getTemperature();
-
-  readBatteryLevel();
-
-     Serial.print("Temp: ");
- Serial.println(temp_f);
- Serial.print("Humidity: ");
- Serial.println(humidity);
- Serial.print("Voltage: ");
- Serial.println(batteryLevel);
-
- 
   Serial.print("connecting to ");
   Serial.println(host);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 8080;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
@@ -90,38 +115,53 @@ void readAllAndReport() {
   Serial.print("POSTING to URL: ");
   Serial.println(url);
 
-  String messageBody = "{\"temp\": ";
-  messageBody += temp_f;
-  messageBody += ", \"humidity\": ";
-  messageBody += humidity;
-  messageBody += ", \"voltage\": ";
-  messageBody += batteryLevel;
-  messageBody += "}\r\n";       
+  String messageBody = String("{\"temps\": ") 
+    + arrayToJson(temps) + ", \"humidity\": " 
+    + arrayToJson(hums) + ", \"battery\": "
+    + arrayToJson(batteryLevels) + "}\r\n";       
+
+  Serial.println(messageBody);
         
   client.print(String("POST ") + url + " HTTP/1.1\r\n" +
         "Host: " + hostName + "\r\n" +
         "Content-Type: application/json\r\n" +
         "Content-Length: " +
         String(messageBody.length()) + "\r\n\r\n");
-    client.print(messageBody);
+  client.print(messageBody);
+
+  // wait for the server to do its thing
   delay(500);
   
   // Read all the lines of the reply from server and print them to Serial
   while(client.available()){
     String line = client.readStringUntil('\r');
-    Serial.print(line);
+    Serial.println(line);
   }
   
   Serial.println();
   Serial.println("closing connection");
 }
- 
-int value = 0;
- 
-void loop() {
-  delay(5000);
-  ++value;
 
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+#ifndef BATTERY_TEST
+  // We start by connecting to a WiFi network
   readAllAndReport();
 
+  Serial.println("Zzzz...");
+  ESP.deepSleep(POLL_INTERVAL_MICROSECONDS, WAKE_RF_DEFAULT);
+#endif
+}
+
+
+void loop() {
+  // never gets here, so do nothing
+#ifdef BATTERY_TEST
+  readAllAndReport();
+  delay(60000);
+#endif
 }
