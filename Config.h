919b7801448ce29619cb0090986ceb2125f007cb
Tom McKearney <thomas.mckearney@appliedis.com>
2016-06-03 16:28:38 -0400

    Moving Config to a Config class w/ static members
    Centralizing config in one structure

---
+++ Config.h
@@ -0,0 +1,23 @@
+
+struct Config {
+    static const uint8_t codeVersion = 0x08; // Change/Increment this each version
+
+    // Networking
+    static constexpr char* ssid     = "McKearneys";
+    static constexpr char* password = "";
+    static constexpr char* hostName = "toms-office"; // TODO: CHANGE FOR EACH DEVICE!
+    static constexpr char* serverHost = "192.168.0.106";
+    static constexpr int serverPort = 8080;
+
+    // sensor config
+    static const int dhtPin = 12;
+    static const bool hasSoilTemp = false;
+    static const int ds18b20Pin = 4;
+
+    // app config
+    static const bool shouldSleep = false;
+    static const int minutesBetweenMeasurements = 1;
+    static const int numTimesPerReading = 5; // times to read sensor per reading
+    static const int numReadingsUntilSend = 30;
+};
+
--- esp8266TempHumidity.ino
+++ esp8266TempHumidity.ino
@@ -13,31 +13,18 @@
 #include "Utils.h"
 #include "Ds18b20.h"
 
-const char* ssid     = "McKearneys";
-const char* password = "";
-const char* hostName = "hoopy-outside"; // TODO: CHANGE FOR EACH DEVICE!
-#define HAS_SOIL_TEMP 1
- 
-const char* host = "192.168.0.106";
-const int httpPort = 8080;
+#include "Config.h"
 
-#define DHTPIN  12
 #define MICROSECONDS_PER_MINUTE 60*1000000  // 60 seconds * 1 million 
-
-const int minutesBetweenMeasurements = 1;
-const int numTimesPerReading = 5; // times to read sensor per reading
-const int numReadingsUntilSend = 30;
-
-const uint8_t CODE_VERSION = 0x07; // Change/Increment this each version
 
 struct Readings {
   uint8_t marker; // indicator that we're reading proper data
   uint8_t readingsFinished;
 
-  int temperature[numReadingsUntilSend];
-  int humidity[numReadingsUntilSend];
-  int batteryLevel[numReadingsUntilSend];
-  int soilTemperature[numReadingsUntilSend];
+  int temperature[Config::numReadingsUntilSend];
+  int humidity[Config::numReadingsUntilSend];
+  int batteryLevel[Config::numReadingsUntilSend];
+  int soilTemperature[Config::numReadingsUntilSend];
 };
 
 // forward declarations
@@ -47,40 +34,38 @@ String arrayToJson(int* arr, int length);
 void SendData(Readings& data);
 void ReadValues(int index, Readings& storedData);
 
-#define NO_SLEEP 1
-
 // temp/humidity + battery
-Dht22 dht22(DHTPIN);
-Battery battery(A0);
+Dht22 dht22(Config::dhtPin);
+Battery battery(A0); // TODO: config?
 
 #ifdef HAS_SOIL_TEMP
-Ds18b20 soilTemp(4);
+Ds18b20 soilTemp(Config::ds18b20Pin);
 #endif
 
 // Using Blue LED for debugging
 Led led(2, true); // pin 2 needs inverting
 
 void loop() {
-#ifdef NO_SLEEP
-  Serial.println("Next Batch");
-  Readings data;
-  InitializeStoredData(data);
-  for(int i=0; i < numReadingsUntilSend; ++i) {
-    elapsedMillis sinceStart;
-    led.OnOff(true);
-    ReadValues(i, data);
-    led.OnOff(false);
-    
-    Serial.print(F("Took this many millis: "));
-    Serial.println(sinceStart);
-
-    // sleep for proper minutes minus processing time
-    uint32_t millisDelay = minutesBetweenMeasurements * 60000 - sinceStart;
-
-    Utils::Delay(millisDelay);
+  if (Config::shouldSleep == false) {
+    Serial.println("Next Batch");
+    Readings data;
+    InitializeStoredData(data);
+    for(int i=0; i < Config::numReadingsUntilSend; ++i) {
+      elapsedMillis sinceStart;
+      led.OnOff(true);
+      ReadValues(i, data);
+      led.OnOff(false);
+      
+      Serial.print(F("Took this many millis: "));
+      Serial.println(sinceStart);
+  
+      // sleep for proper minutes minus processing time
+      uint32_t millisDelay = Config::minutesBetweenMeasurements * 60000 - sinceStart;
+  
+      Utils::Delay(millisDelay);
+    }
+    SendData(data);
   }
-  SendData(data);
-#endif
 }
 
 void setup() {
@@ -88,73 +73,71 @@ void setup() {
   Utils::Delay(100);
 
   Serial.print(F("\nHoopy name: "));
-  Serial.print(hostName);
+  Serial.print(Config::hostName);
   Serial.print(F(" running version: "));
-  Serial.println(CODE_VERSION);
-  
-#ifndef NO_SLEEP
-  
-  elapsedMillis sinceStart;
-  EEPROM.begin(4096);
+  Serial.println(Config::codeVersion);
 
-  // First, read the existing readings from the EEPROM
-  Readings storedData;
-  EEPROM.get(0, storedData);
-  if (storedData.marker != CODE_VERSION) {
-    Serial.println(F("No Readings marker, initializing"));
-    InitializeStoredData(storedData);
+  if (Config::shouldSleep == true) {
+      elapsedMillis sinceStart;
+      EEPROM.begin(4096);
+    
+      // First, read the existing readings from the EEPROM
+      Readings storedData;
+      EEPROM.get(0, storedData);
+      if (storedData.marker != Config::codeVersion) {
+        Serial.println(F("No Readings marker, initializing"));
+        InitializeStoredData(storedData);
+      }
+      
+      int newIndex = storedData.readingsFinished;
+      ReadValues(newIndex, storedData);
+    
+      if (newIndex == Config::numReadingsUntilSend-1) {
+    
+        Serial.println(F("Last Reading: uploading!"));
+        // this is the last reading before send, so send now
+        SendData(storedData);
+    
+        InitializeStoredData(storedData);
+      }
+      else {
+        ++storedData.readingsFinished;
+      }
+    
+      // save the structure back
+      // TODO: do this piecemeal, rather than the whole structure at once
+      EEPROM.put(0, storedData);
+      EEPROM.end();
+      
+      Serial.print(F("Sleeping for "));
+      Serial.print(Config::minutesBetweenMeasurements);
+      Serial.println(F(" minutes. Zzzz..."));
+      led.OnOff(false);
+    
+      Serial.print(F("Took this many millis: "));
+      Serial.println(sinceStart);
+    
+      WakeMode wakeMode = WAKE_RF_DISABLED;
+      if (storedData.readingsFinished == Config::numReadingsUntilSend-1) {
+        // wake up w/ Wifi next time
+        wakeMode = WAKE_RF_DEFAULT;
+      }
+    
+      // sleep for proper minutes minus processing time
+      uint32_t timeToSleep = Config::minutesBetweenMeasurements * MICROSECONDS_PER_MINUTE - sinceStart * 1000;
+      
+      ESP.deepSleep(timeToSleep, wakeMode);
   }
-
-  
-  int newIndex = storedData.readingsFinished;
-  ReadValues(newIndex, storedData);
-
-  if (newIndex == numReadingsUntilSend-1) {
-
-    Serial.println(F("Last Reading: uploading!"));
-    // this is the last reading before send, so send now
-    SendData(storedData);
-
-    InitializeStoredData(storedData);
-  }
-  else {
-    ++storedData.readingsFinished;
-  }
-
-  // save the structure back
-  // TODO: do this piecemeal, rather than the whole structure at once
-  EEPROM.put(0, storedData);
-  EEPROM.end();
-  
-  Serial.print(F("Sleeping for "));
-  Serial.print(minutesBetweenMeasurements);
-  Serial.println(F(" minutes. Zzzz..."));
-  led.OnOff(false);
-
-  Serial.print(F("Took this many millis: "));
-  Serial.println(sinceStart);
-
-  WakeMode wakeMode = WAKE_RF_DISABLED;
-  if (storedData.readingsFinished == numReadingsUntilSend-1) {
-    // wake up w/ Wifi next time
-    wakeMode = WAKE_RF_DEFAULT;
-  }
-
-  // sleep for proper minutes minus processing time
-  uint32_t timeToSleep = minutesBetweenMeasurements * MICROSECONDS_PER_MINUTE - sinceStart * 1000;
-  
-  ESP.deepSleep(timeToSleep, wakeMode);
-#endif
 }
 
 ///////// IMPLEMENTATION below
 
 
 void InitializeStoredData(Readings& data) {
-    data.marker = CODE_VERSION;
+    data.marker = Config::codeVersion;
     data.readingsFinished = 0;
 
-    for(int i=0; i < numReadingsUntilSend; ++i) {
+    for(int i=0; i < Config::numReadingsUntilSend; ++i) {
       data.temperature[i] = -1;
       data.humidity[i] = -1;
       data.batteryLevel[i] = -1;
@@ -164,20 +147,20 @@ void InitializeStoredData(Readings& data) {
 void ReadValues(int index, Readings& storedData) {
     // read new values
     float
-        temperature[numTimesPerReading],
-        humidity[numTimesPerReading],
-        batteryLevels[numTimesPerReading];
+        temperature[Config::numTimesPerReading],
+        humidity[Config::numTimesPerReading],
+        batteryLevels[Config::numTimesPerReading];
 
     Serial.print(F("Reading Values for index: "));
     Serial.println(index);
 
     // read and normalize values
-    dht22.ReadTempAndHumidity(numTimesPerReading, temperature, humidity);
-    int temp = normalizeReadings(temperature, numTimesPerReading);
-    int hum = normalizeReadings(humidity, numTimesPerReading);
+    dht22.ReadTempAndHumidity(Config::numTimesPerReading, temperature, humidity);
+    int temp = normalizeReadings(temperature, Config::numTimesPerReading);
+    int hum = normalizeReadings(humidity, Config::numTimesPerReading);
 
-    battery.ReadLevels(numTimesPerReading, batteryLevels);
-    int batt = normalizeReadings(batteryLevels, numTimesPerReading);
+    battery.ReadLevels(Config::numTimesPerReading, batteryLevels);
+    int batt = normalizeReadings(batteryLevels, Config::numTimesPerReading);
 
     int soilTemperature = -1;
     #ifdef HAS_SOIL_TEMP
@@ -221,9 +204,9 @@ String arrayToJson(int* arr, int length)
 
 void SendData(Readings& data) {
     Serial.print(F("\nConnecting to "));
-    Serial.println(ssid);
+    Serial.println(Config::ssid);
   
-    WiFi.begin(ssid, "");
+    WiFi.begin(Config::ssid, Config::password);
 
   Serial.println(F("After Begin"));
   
@@ -237,11 +220,11 @@ void SendData(Readings& data) {
   Serial.println(WiFi.localIP());
 
   Serial.print(F("connecting to "));
-  Serial.println(host);
+  Serial.println(Config::serverHost);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  if (!client.connect(host, httpPort)) {
+  if (!client.connect(Config::serverHost, Config::serverPort)) {
     Serial.println(F("connection failed"));
     return;
   }
@@ -251,17 +234,17 @@ void SendData(Readings& data) {
   Serial.print(F("POSTING to URL: "));
   Serial.println(url);
 
-  String messageBody = String(F("{\"version\": ")) + CODE_VERSION 
-    + F(", \"temps\": ") + arrayToJson(data.temperature, numReadingsUntilSend) 
-    + F(", \"humidity\": ") + arrayToJson(data.humidity, numReadingsUntilSend) 
-    + F(", \"battery\": ") + arrayToJson(data.batteryLevel, numReadingsUntilSend) 
-    + F(", \"soilTemp\": ") + arrayToJson(data.soilTemperature, numReadingsUntilSend) 
+  String messageBody = String(F("{\"version\": ")) + Config::codeVersion 
+    + F(", \"temps\": ") + arrayToJson(data.temperature, Config::numReadingsUntilSend) 
+    + F(", \"humidity\": ") + arrayToJson(data.humidity, Config::numReadingsUntilSend) 
+    + F(", \"battery\": ") + arrayToJson(data.batteryLevel, Config::numReadingsUntilSend) 
+    + F(", \"soilTemp\": ") + arrayToJson(data.soilTemperature, Config::numReadingsUntilSend) 
     + F("}\r\n");
 
   Serial.println(messageBody);
         
   client.print(String(F("POST ")) + url + F(" HTTP/1.1\r\n") +
-        F("Host: ") + hostName + F("\r\n") +
+        F("Host: ") + Config::hostName + F("\r\n") +
         F("Content-Type: application/json\r\n") +
         F("Content-Length: ") +
         String(messageBody.length()) + F("\r\n\r\n"));
